e30debf6bb0ca3dc84f3532f2779c0bae65103f3
heyynat <natalifplima@gmail.com>
2022-04-09 18:49:07 -0400

    Ruby: Conceitos Básicos - Aula 1 e 2

---
+++ hello.rb
@@ -0,0 +1,12 @@
+puts 'Hello OneBitCode'
+
+# Saída de dados
+print 'Digite seu nome: '
+
+# Recebendo uma entrada do teclado
+name = gets.chomp
+
+# Saída utilizando puts
+# use códigos Ruby dentro de uma string com #{code}
+
+puts "Hello #{name}!"
